a0122389f44a575cb079a9c18215786d47a7d410
hugoibarrae <72998850+hugoibarrae@users.noreply.github.com>
2021-02-08 07:30:43 -0600

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,66 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6IKu89PQGFO":
+        Script1();
+        break;
+      case "5mf60PFJVSi":
+        Script2();
+        break;
+      case "62Q91QzvifL":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+head.appendChild(script)
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+ 
+//PLACE YOUR WEB APP URL
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbykZs37uASMOOWplQHt5lLZB7vqpC6OM4in9FPX0ge8Kd3NsgY/exec";
+ 
+// STORE ARTICULATE STORYLINE VARIABLES
+// "Columnname_Google_Spreadsheet" : player.GetVar("Name_Storyline_Variable")
+// ATTENTION: Use a comma if you use multiple Storyline variables
+storyline =
+{
+ "date" : new Date().toJSON().slice(0,10), //STORE DATE
+ "name" : player.GetVar("nombre"),
+ "email" : player.GetVar("mail"),
+ "message" : player.GetVar("message")
+}
+}
+
+function Script3()
+{
+  //DELAY SO JQUERY LIBRARY IS LOADED
+setTimeout(function (){
+ 
+//Export to Google
+$.ajax({
+url: WEB_APP_URL,
+type: "POST",
+data : storyline,
+success: function(data)
+{
+console.log(data);
+},
+error: function(err) {
+console.log('Error:', err);
+}
+});
+return false;
+}, 1000);
+}
+
